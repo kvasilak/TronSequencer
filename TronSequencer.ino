5a32c431ef4dca1b92a838bf9951b3dcc98bf731
kvasilakes <my427v8@hotmail.com>
2018-09-07 00:15:41 -0500

    led test

--- TronSequencer.ino
+++ TronSequencer.ino
@@ -7,19 +7,12 @@
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10 (CE & CS)
 RF24 radio(9,10);
 
-// Pins on the remote for buttons
-//const uint8_t button_pins[] = { 2,3,4,5,6,7 };
-//const uint8_t num_button_pins = sizeof(button_pins);
 
 // Pins on the LED board for LED's
-const uint8_t led_pins[] = { 2,3,4,5,6,7 };
-//const uint8_t num_led_pins = sizeof(led_pins);
+const uint8_t led_pins[] = { 2,3,4,5,6,7,8,9 };
 
 // Single radio pipe address for the 2 nodes to communicate.
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-
-//uint8_t button_states[num_button_pins];
-//uint8_t led_states[num_led_pins];
 
 uint8_t state = 0;
 
@@ -41,42 +34,52 @@ void setup(void)
   radio.printDetails();
 
   // Turn EL Off
-    int i = sizeof(led_pins);
-    while(i--)
+    int i;
+    for(i=0; i<sizeof(led_pins); i++)
     {
       pinMode(led_pins[i],OUTPUT);
-      //led_states[i] = LOW;
-      digitalWrite(led_pins[i],LOW); //led_states[i]);
+      digitalWrite(led_pins[i],HIGH); 
     }
 }
 
 void loop(void)
 {
-    delay(4);
+  int i;
+  for(i=0; i<sizeof(led_pins); i++)
+  {
+    digitalWrite(led_pins[i],LOW);
 
-    // if there is data ready
-    if ( radio.available() )
-    {
-      // Read Data from the radio
-      while (radio.available())
-      {
-        radio.read( &state, 1 );
-        printf("State change %d\n\r", state);
+    delay(500);
 
-        if(state==0)
-        {
-          digitalWrite(led_pins[0],LOW);
-          digitalWrite(led_pins[1],LOW);
-          digitalWrite(led_pins[2],LOW);
-        }
-        else if(state==1)
-        {
-          digitalWrite(led_pins[0],HIGH);
-          digitalWrite(led_pins[1],HIGH);
-          digitalWrite(led_pins[2],HIGH);
-        }
-      }
-    }
+     digitalWrite(led_pins[i],HIGH);
+
+  }
+  
+//   delay(4); 
+//
+//    // if there is data ready
+//    if ( radio.available() )
+//    {
+//      // Read Data from the radio
+//      while (radio.available())
+//      {
+//        radio.read( &state, 1 );
+//        printf("State change %d\n\r", state);
+//
+//        if(state==0)
+//        {
+//          digitalWrite(led_pins[0],LOW);
+//          digitalWrite(led_pins[1],LOW);
+//          digitalWrite(led_pins[2],LOW);
+//        }
+//        else if(state==1)
+//        {
+//          digitalWrite(led_pins[0],HIGH);
+//          digitalWrite(led_pins[1],HIGH);
+//          digitalWrite(led_pins[2],HIGH);
+//        }
+//      }
+//    }
 }
 
 void AllOff(void)
