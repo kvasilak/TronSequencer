0588aec1535bf9ce72aaafb127af3c6f093837b2
kvasilakes <my427v8@hotmail.com>
2018-09-30 22:42:39 -0500

    correct sequences

--- GraceSequencer.ino
+++ GraceSequencer.ino
@@ -4,22 +4,25 @@
 #include "RF24_config.h"
 #include <SPI.h>
 
-#define LeftLeg 0
-#define LeftBody 1
-#define LeftArm 2
+#define LeftLeg 5
+#define LeftBody 6
+#define LeftArm 7
 
-#define RightLeg 3
-#define RightArm 4
-#define RightBody 5
+#define RightLeg 2
+#define RightBody 1
+#define RightArm 0
 
-#define Head 6 
-#define Eyes 7
+#define Head 3
+#define Eyes 4
+
+#define On HIGH
+#define Off LOW
 
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10 (CE & CS)
-RF24 radio(9,10);
+RF24 radio(15,10);
 
 // EL Enable Pins on the EL Sequencer
-const uint8_t ElWire[] = { 2,3,4,5,6,7,8,13 };//todo get pins for sequencer
+const uint8_t ElWire[] = { 2,3,4,5,6,7,8,9 };//todo get pins for sequencer
 
 // Single radio pipe address for the 2 nodes to communicate.
 const uint64_t pipe = 0xE8E8F0F0E1LL;
@@ -28,6 +31,8 @@ uint8_t state = 0;
 uint8_t OldState = 0;
 
 void AllOff(void);
+void Sequence1(void);
+void AllOn(void);
 void PowerUp(void);
 void PowerDown(void);
 void MotorCycle(void);
@@ -36,11 +41,18 @@ void GraceOn(void);
 void Wave(void);
 
 typedef void (* GenericFP)(void);
-GenericFP StateFuncs[] = {AllOff,PowerUp,PowerDown,MotorCycle,KeithOn,GraceOn, Wave};
-
-const uint8_t Grace = 1;
-const uint8_t Keith = 2;
-const uint8_t me = Grace;
+GenericFP StateFuncs[] = {AllOff, //Grace
+                          PowerUp, // 1 both power on
+                          AllOn, // 2 keith off ships & motorcycles
+                          AllOn, // 3 keith on
+                          AllOn, // 4 Keith off, carry and drop
+                          AllOn, // 5 Keith On
+                          AllOff,// 6 grace off, keith on solo
+                          AllOn, // 7 keith off, grace on solo
+                          AllOn, // 8 keith on
+                          AllOn, // 9 keith off, grace back bend
+                          AllOn, // 10 keith on
+                          PowerDown}; // 11 power down
 
 void setup(void)
 {
@@ -58,6 +70,13 @@ void setup(void)
   radio.openReadingPipe(1,pipe);
   radio.startListening();
   radio.printDetails();
+  
+  int i;
+  for(i=0;i<sizeof(ElWire); i++)
+  {
+    pinMode(ElWire[i],OUTPUT);
+    digitalWrite(ElWire[i],HIGH);
+  }
 
   AllOff();
 }
@@ -69,22 +88,27 @@ void loop(void)
     // if there is data ready
     if ( radio.available() )
     {
+     // printf("Radio Avail\r\n");
+      
       // Read Data from the radio
       while (radio.available())
       {
+        //printf("Radio read\r\n");
         radio.read( &state, 1 );
+
+        //printf("Radio read 1\r\n");
         
         if(state != OldState)
         {
             if(state < sizeof(StateFuncs))
             {
                 OldState = state;
-                StateFuncs[state](); //call state function
-                printf("State Change: %d\r\n");
+                //printf("State Change: %d\r\n", state);
+                StateFuncs[state](); //call state function 
             }
             else
             {
-                printf("Unknown State: %d\r\n");
+                printf("Unknown State: %d\r\n", state);
             }
                 
         }
@@ -94,13 +118,53 @@ void loop(void)
 
 void AllOff(void)
 {
+    printf("All Off\r\n");
+  
     // Turn EL Off
     int i;
     for(i=0;i<sizeof(ElWire); i++)
     {
-      pinMode(ElWire[i],OUTPUT);
+      digitalWrite(ElWire[i],LOW);
+    }
+}
+
+void AllOn(void)
+{
+    printf("All On\r\n");
+    // Turn EL Off
+    int i;
+    for(i=0;i<sizeof(ElWire); i++)
+    {
       digitalWrite(ElWire[i],HIGH);
     }
+}
+
+void Sequence(void)
+{
+   printf("Sequence\r\n");
+    // Turn EL Off
+    int i;
+    for(i=0;i<sizeof(ElWire); i++)
+    {
+      digitalWrite(ElWire[i],HIGH);
+      delay(500);
+    }
+}
+
+void Sequence1(void)
+{
+    AllOff();delay(500);
+  
+    digitalWrite(ElWire[RightLeg],On);delay(500);
+    digitalWrite(ElWire[RightBody],On);delay(500);
+    digitalWrite(ElWire[RightArm],On);delay(500);
+    
+    digitalWrite(ElWire[LeftLeg],On);delay(500);
+    digitalWrite(ElWire[LeftBody],On);delay(500);
+    digitalWrite(ElWire[LeftArm],On);delay(500);
+
+    digitalWrite(ElWire[Head],On);delay(500);
+    digitalWrite(ElWire[Eyes],On);
 }
 
 //lright leg on, right bodyon
@@ -110,60 +174,64 @@ void AllOff(void)
 // the power up sequence is 3 seconds long
 void PowerUp(void)
 {
-    digitalWrite(ElWire[LeftLeg],LOW);
-    delay(100);
-    digitalWrite(ElWire[RightLeg],LOW);
+  AllOff();
+  
+    printf("PowerUp\r\n");
+    digitalWrite(ElWire[LeftLeg],On);
     delay(200);
+    digitalWrite(ElWire[RightLeg],On);
+    delay(300);
     
-    digitalWrite(ElWire[LeftBody],LOW);
-    delay(100);
-    digitalWrite(ElWire[RightBody],LOW);
-    delay(500);
+    digitalWrite(ElWire[LeftBody],On);
+    delay(200);
+    digitalWrite(ElWire[RightBody],On);
+    delay(300);
     
     //arms
-    digitalWrite(ElWire[LeftArm],LOW);
-    delay(100);
-    digitalWrite(ElWire[LeftArm],LOW);
-    delay(100);
-    digitalWrite(ElWire[LeftArm],HIGH);
-    delay(100);
-    digitalWrite(ElWire[LeftArm],HIGH);
-    delay(100);
-    digitalWrite(ElWire[LeftArm],LOW);
-    digitalWrite(ElWire[LeftArm],LOW);
-    delay(210);
+    digitalWrite(ElWire[LeftArm],On);
+    delay(200);
+    digitalWrite(ElWire[RightArm],On);
+    delay(200);
+    digitalWrite(ElWire[LeftArm],Off);
+    delay(200);
+    digitalWrite(ElWire[RightArm],Off);
+    delay(200);
+    digitalWrite(ElWire[LeftArm],On);
+    digitalWrite(ElWire[RightArm],On);
+    delay(300);
     
-    digitalWrite(ElWire[Head],LOW);
+    digitalWrite(ElWire[Head],On);
+    delay(200);
+    digitalWrite(ElWire[Eyes],On);
     delay(100);
-    digitalWrite(ElWire[Eyes],LOW);
-    delay(100);
-    digitalWrite(ElWire[Eyes],HIGH);
-    delay(100);
-    digitalWrite(ElWire[Eyes],LOW);
+    digitalWrite(ElWire[Eyes],Off);
+    delay(200);
+    digitalWrite(ElWire[Eyes],On);
 }
 
 void BodyPwrDwn(void)
 {
+   printf("BodyPwrDwn\r\n");
+  
     int i;
-    
     for(i=0;i<10;i++)
     {
-        digitalWrite(ElWire[LeftLeg],HIGH);
-        digitalWrite(ElWire[RightLeg],HIGH);
+        digitalWrite(ElWire[LeftLeg],On);
+        digitalWrite(ElWire[RightLeg],On);
        
-        digitalWrite(ElWire[LeftBody],HIGH);
-        digitalWrite(ElWire[RightBody],HIGH);
-        digitalWrite(ElWire[LeftArm],HIGH);
-        digitalWrite(ElWire[LeftArm],HIGH);
-        delay(i*10); //on more and more each loop
+        digitalWrite(ElWire[LeftBody],On);
+        digitalWrite(ElWire[RightBody],On);
+        digitalWrite(ElWire[LeftArm],On);
+        digitalWrite(ElWire[RightArm],On);
+        delay(i*20); //on more and more each loop
         
-        digitalWrite(ElWire[LeftLeg],LOW);
-        digitalWrite(ElWire[RightLeg],LOW);
+        digitalWrite(ElWire[LeftLeg],Off);
+        digitalWrite(ElWire[RightLeg],Off);
        
-        digitalWrite(ElWire[LeftBody],LOW);
-        digitalWrite(ElWire[RightBody],LOW);
-        digitalWrite(ElWire[LeftArm],LOW);
-        digitalWrite(ElWire[LeftArm],LOW);
+        digitalWrite(ElWire[LeftBody],Off);
+        digitalWrite(ElWire[RightBody],Off);
+        digitalWrite(ElWire[LeftArm],Off);
+        digitalWrite(ElWire[RightArm],Off);
         delay(100-(i*10));//off more and more each loop
     }
 }
@@ -174,59 +242,36 @@ void BodyPwrDwn(void)
 //then turn eyes off, delay, then back on briefly
 void PowerDown(void)
 {
+  printf("PowerDown\r\n");
+  
   BodyPwrDwn();
   
-  digitalWrite(ElWire[Head],HIGH);
+  digitalWrite(ElWire[Head],Off);
   delay(100);
-  digitalWrite(ElWire[Head],LOW);
-  delay(500);
-  digitalWrite(ElWire[Eyes],HIGH);
+  digitalWrite(ElWire[Head],On);
+  delay(200);
+  digitalWrite(ElWire[Eyes],Off);
   delay(100);
-  digitalWrite(ElWire[Eyes],LOW);
-  delay(500);
-  digitalWrite(ElWire[Head],HIGH);
-  delay(500);
-  digitalWrite(ElWire[Eyes],HIGH);
-  delay(500);
-  digitalWrite(ElWire[Eyes],LOW);
-  delay(500);
-  digitalWrite(ElWire[Eyes],HIGH);
+  digitalWrite(ElWire[Eyes],On);
+  delay(200);
+  digitalWrite(ElWire[Head],Off);
+  delay(200);
+  digitalWrite(ElWire[Eyes],Off);
+  delay(200);
+  digitalWrite(ElWire[Eyes],On);
+  delay(200);
+  digitalWrite(ElWire[Eyes],Off);
   delay(1000);
-  digitalWrite(ElWire[Eyes],LOW);
+  digitalWrite(ElWire[Eyes],On);
   delay(100);
-  digitalWrite(ElWire[Eyes],HIGH);
-}
-
-void MotorCycle(void)
-{
-  KeithOff();
-  GraceOn();
-}
-
-void KeithOn(void)
-{
-  
-}
-
-void GraceOn(void)
-{
-  
-}
-
-void KeithOff(void)
-{
-  
-}
-
-void GraceOff(void)
-{
-  
+  digitalWrite(ElWire[Eyes],Off);
 }
 
 #define WaveDelay 100
 
 void WaveRight(bool show)
 {
+  printf("WaveRight\r\n");
     if( show)digitalWrite(ElWire[RightArm],HIGH);
     delay(WaveDelay);
     if( show)digitalWrite(ElWire[RightArm],LOW);
@@ -258,6 +303,7 @@ void WaveRight(bool show)
 
 void WaveLeft(bool show)
 {
+  printf("WaveLeft\r\n");
     delay(WaveDelay);
     if( show)(ElWire[LeftArm],HIGH);
     if( show)(ElWire[LeftArm],HIGH); 
@@ -292,13 +338,14 @@ void WaveLeft(bool show)
 //start all on, send off wave through us as we do the wave
 void Wave(void)
 {
+  printf("Wave\r\n");
     //starts on Graces Right
-    WaveRight(me == Grace);//Graces version
-    WaveRight(me == Keith);//Keiths version
+//    WaveRight(me == Grace);//Graces version
+//    WaveRight(me == Keith);//Keiths version
     
     //and back again
-    WaveLeft(me == Grace);
-    WaveLeft(me == Keith);
+//    WaveLeft(me == Grace);
+//   WaveLeft(me == Keith);
  
 }
 
